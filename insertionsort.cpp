ead42c6c84ed3f9d9274549a2166ddd65cad0f55
Sourav Saha <150452721+sourav98765@users.noreply.github.com>
2024-10-31 00:07:56 +0600

    Add files via upload

---
+++ insertionsort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void print(int arr[], int n) { 
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void insertionsort (int arr[], int n){
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i-1;
+        while (prev >= 0 && arr[prev] > curr)
+        {
+           swap(arr[prev],arr[prev+1]);
+           prev -- ;
+        }
+        print(arr,n);
+    }
+    
+}
+int main (){
+    int arr[5]={5,4,1,3,2};
+    insertionsort(arr,5);
+    return 0;
+}
